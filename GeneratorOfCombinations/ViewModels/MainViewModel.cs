18d3fc8891b9046cf6309676044dd8ca53598120
Влад Концевич <kontsevichv@mail.ru>
2021-02-25 14:10:07 +0300

    User interface developed

--- GeneratorOfCombinations/ViewModels/MainViewModel.cs
+++ GeneratorOfCombinations/ViewModels/MainViewModel.cs
@@ -1,24 +1,59 @@
-﻿namespace GeneratorOfCombinations
+﻿using System.Collections.Generic;
+using System.Windows.Input;
+
+namespace GeneratorOfCombinations
 {
 	public class MainViewModel : BaseViewModel
 	{
 		#region Public Properties
 
+		/// <summary>
+		/// A set of elements 
+		/// </summary>
+		public string Set { get; set; }
+
+		/// <summary>
+		/// Combination size
+		/// </summary>
+		public string CombinationSize { get; set; }
+
+		/// <summary>
+		/// Possible combinations of m elements from the set of element with size n. 
+		/// </summary>
+		public List<string> Combinations { get; set; }
+
 		#endregion
 
 		#region Commands
+
+		/// <summary>
+		/// The command to сalculate сombinations
+		/// </summary>
+		public ICommand CalculateCommand { get; set; }
 
 		#endregion
 
 		#region Constructor
 
+		/// <summary>
+		/// Default constructor
+		/// </summary>
 		public MainViewModel()
 		{
+			// Create commands
+			CalculateCommand = new RelayCommand(CalculateCombinations);
 		}
 
 		#endregion
 
 		#region Command Methods
+
+		/// <summary>
+		/// Calculate сombinations
+		/// </summary>
+		public void CalculateCombinations()
+		{
+		}
 
 		#endregion
 	}
